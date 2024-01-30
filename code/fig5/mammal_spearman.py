2f3d31bd2bf70a55dc2a94003421844105f382ae
Sota-Nakashima <souta.nakashima2001@gmail.com>
2024-01-30 16:06:47 +0900

    9th commit

--- code/fig5/mammal_spearman.py
+++ code/fig5/mammal_spearman.py
@@ -1,10 +1,16 @@
 #ライブラリのインポート
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 
 #データの読み込み
 df = pd.read_csv("data/csv/spearman.csv")
+
+#yearをlog1p変換
+df.loc[
+    df["taxon"] == "mammal", 'year'
+    ] = np.log1p(df.loc[df["taxon"] == "mammal", 'year'])
 
 #mammalのデータのみ抜き出す
 df_pict = df[df["taxon"] == "mammal"]
@@ -12,13 +18,15 @@ df_pict = df[df["taxon"] == "mammal"]
 #信頼区間つき折れ線グラフ
 sns.relplot(
     x = "year",y = "value",
-    data=df_pict,kind="line",hue="RNAkind",palette=["#0071BC","#FF5050"])
+    data=df_pict,kind="line",hue="RNAkind",palette=["#0071BC","#FF5050"]
+    )
 
 #細かい調整
-plt.xticks([94,0,6.4,28.8,87],rotation=45) 
+plt.xticks(np.log1p([0,6.4,28.8,87]),[0,6.4,28.8,87],rotation=45) 
 plt.xlabel("Divergence Time (MYA)",fontsize = "14")
 plt.ylabel("Spearmans'ρ",fontsize = "14")
 plt.title("Mammal",fontsize="18")
+
 #保存
 #bbox_inchesはタイトルの見切れ防止
 plt.savefig("output/fig5/mammal_spearman.pdf",bbox_inches='tight')
--- code/fig6/drosophila_spearman.py
+++ code/fig6/drosophila_spearman.py
@@ -1,10 +1,16 @@
 #ライブラリのインポート
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 
 #データの読み込み
 df = pd.read_csv("data/csv/spearman.csv")
+
+#yearをlog1p変換
+df.loc[
+    df["taxon"] == "drosophila", 'year'
+    ] = np.log1p(df.loc[df["taxon"] == "drosophila", 'year'])
 
 #drosophilaのデータのみ抜き出す
 df_pict = df[df["taxon"] == "drosophila"]
@@ -12,10 +18,11 @@ df_pict = df[df["taxon"] == "drosophila"]
 #信頼区間つき折れ線グラフ
 sns.relplot(
     x = "year",y = "value",
-    data=df_pict,kind="line",hue="RNAkind",palette=["#0071BC","#FF5050"])
+    data=df_pict,kind="line",hue="RNAkind",palette=["#0071BC","#FF5050"]
+    )
 
 #細かい調整
-plt.xticks([36,0,33,3.95,54,11.6],rotation=45) 
+plt.xticks(np.log1p([36,0,3.95,54,11.6]),[36,0,3.95,54,11.6],rotation=45)
 plt.xlabel("Divergence Time (MYA)",fontsize = "14")
 plt.ylabel("Spearmans'ρ",fontsize = "14")
 plt.title("Drosophila",fontsize="18")
